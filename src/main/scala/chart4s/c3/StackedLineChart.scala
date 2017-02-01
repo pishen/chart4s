b32e34a7d9846195fbc5e36b74b40b7fe269f038
pishen tsai <pishen02@gmail.com>
2017-02-01 10:12:49 +0800

    add stacked line chart

---
+++ src/main/scala/chart4s/c3/StackedLineChart.scala
@@ -0,0 +1,23 @@
+package chart4s.c3
+
+import chart4s.Decimal
+import play.api.libs.json._
+
+case class StackedLineChart[A, B](
+  lines: Seq[(String, Seq[A])],
+  xValues: Seq[B]
+)(implicit da: Decimal[A], db: Decimal[B]) extends Chart {
+  def json = Json.obj(
+    "bindTo" -> "#chart",
+    "data" -> Json.obj(
+      "x" -> "x",
+      "columns" -> JsArray(
+        JsArray(JsString("x") +: xValues.map(db.toBigDecimal).map(JsNumber.apply)) +: lines.map {
+          case (label, line) => JsArray(JsString(label) +: line.map(da.toBigDecimal).map(JsNumber.apply))
+        }
+      ),
+      "type" -> "area",
+      "groups" -> Json.arr(Json.toJson(lines.map(_._1)))
+    )
+  )
+}
