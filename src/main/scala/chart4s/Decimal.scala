d622c8780505b2c699f6f30488f795eea6b80fdd
pishen tsai <pishen02@gmail.com>
2017-02-15 11:25:53 +0800

    move the default instances of typeclass to its companion object

--- src/main/scala/chart4s/Decimal.scala
+++ src/main/scala/chart4s/Decimal.scala
@@ -3,3 +3,21 @@ package chart4s
 trait Decimal[A] {
   def toBigDecimal(x: A): BigDecimal
 }
+
+object Decimal {
+  implicit val int2BigDecimal = new Decimal[Int] {
+    def toBigDecimal(x: Int) = BigDecimal(x)
+  }
+
+  implicit val long2BigDecimal = new Decimal[Long] {
+    def toBigDecimal(x: Long) = BigDecimal(x)
+  }
+
+  implicit val float2BigDecimal = new Decimal[Float] {
+    def toBigDecimal(x: Float) = BigDecimal.decimal(x)
+  }
+
+  implicit val double2BigDecimal = new Decimal[Double] {
+    def toBigDecimal(x: Double) = BigDecimal(x)
+  }
+}
--- src/main/scala/chart4s/c3/AxisTime.scala
+++ src/main/scala/chart4s/c3/AxisTime.scala
@@ -1,6 +1,21 @@
 package chart4s.c3
 
+import java.time._
+import java.time.format._
+
 trait AxisTime[A] {
   def format(x: A): String
   def pattern: String
+}
+
+object AxisTime {
+  implicit val localDate2AxisTime = new AxisTime[LocalDate] {
+    def format(x: LocalDate) = x.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+    def pattern = "%Y-%m-%d"
+  }
+  
+  implicit val localDateTime2AxisTime = new AxisTime[LocalDateTime] {
+    def format(x: LocalDateTime) = x.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+    def pattern = "%Y-%m-%d %H:%M:%S"
+  }
 }
