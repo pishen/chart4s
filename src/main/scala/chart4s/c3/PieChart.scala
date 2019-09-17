094b3edd996d0ad1fb75fe80ea9788fc09ac188e
Jimin Hsieh <jimin.hsieh.engineer@gmail.com>
2019-09-17 09:35:08 +0800

    Apply `implicitly`

--- src/main/scala/chart4s/c3/PieChart.scala
+++ src/main/scala/chart4s/c3/PieChart.scala
@@ -3,13 +3,13 @@ package chart4s.c3
 import chart4s.Decimal
 import play.api.libs.json._
 
-case class PieChart[A](pie: Seq[(String, A)])(implicit d: Decimal[A]) extends Chart {
+case class PieChart[A: Decimal](pie: Seq[(String, A)]) extends Chart {
   def json = Json.obj(
     "bindTo" -> "#chart",
     "data" -> Json.obj(
       "columns" -> JsArray(
         pie.map { case (label, value) =>
-          Json.arr(label, d.toBigDecimal(value))
+          Json.arr(label, implicitly[Decimal[A]].toBigDecimal(value))
         }
       ),
       "type" -> "pie"
